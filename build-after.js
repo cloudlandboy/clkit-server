f726721e64fd5526719eaf1ebbacacd271014073
clboy <syl@clboy.cn>
2023-11-14 23:27:08 +0800

    fix bug

--- build-after.js
+++ build-after.js
@@ -3,6 +3,7 @@ const { join } = require("path");
 
 
 const distPath = join(__dirname, 'dist');
+copyFileSync(join(__dirname, 'package.json'), join(distPath, 'package.json'));
 const ecosystemConfig = {
     apps: [
         {
@@ -15,7 +16,6 @@ const ecosystemConfig = {
 };
 
 const pm2AppPath = join(__dirname, 'dist_app');
-copyFileSync(join(__dirname, 'package.json'), join(pm2AppPath, 'package.json'));
 cpSync(distPath, pm2AppPath, { force: true, recursive: true })
 
 const pm2Config = `module.exports=${JSON.stringify(ecosystemConfig)}`
